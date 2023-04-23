54e2e8166245b7a0df3693ade433dfad4523a99f
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-04-23 21:29:44 +0300

    update table and main

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	first_name varchar(30) NOT NULL,
+	last_name varchar(30) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+
+CREATE TABLE customers
+(
+	customer_id varchar(30) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(30) REFERENCES customers(customer_id),
+	employee_id int NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,44 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='12345'
+)
+
+try:
+    with conn:
+        # Создается курсор для выполнения операций с базой данных с помощью метода conn.cursor()
+        with conn.cursor() as cur:
+            """Получение данных из таблицы employees_data.csv"""
+            # Открывается файл employees_data.csv с помощью функции open().
+            with open('north_data/employees_data.csv', newline='', encoding='utf-8') as csvfile:
+                # Создается объект csv.reader() для чтения данных из файла CSV с заданными параметрами
+                reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                # С помощью функции next() пропускается первая строка файла CSV, содержащая заголовки столбцов.
+                next(reader)
+                # В цикле for происходит итерация по строкам файла CSV, и для каждой строки выполняется SQL-запрос на
+                # вставку данных в таблицу базы данных с помощью метода cur.execute
+                for row in reader:
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s)", row)
+
+            """Получение данных из таблицы customers_data.csv"""
+            with open('north_data/customers_data.csv', newline='', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                next(reader)
+                for row in reader:
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s)", row)
+
+            """Получение данных из таблицы orders_data.csv"""
+            with open('north_data/orders_data.csv', newline='', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                next(reader)
+                for row in reader:
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s)", row)
+finally:
+    # Закрывается курсор с помощью метода cur.close().
+    # Закрывается соединение с базой данных с помощью метода conn.close()
+    conn.close()
