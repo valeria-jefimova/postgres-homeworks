4445dfa19092f06c3a614dc8e9013326c18311f3
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-04-24 22:51:12 +0300

    updated crate_tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,7 +1,7 @@
 -- SQL-команды для создания таблиц
 CREATE TABLE employees
 (
-	first_name varchar(50) NOT NULL,
+    first_name varchar(50) NOT NULL,
 	last_name varchar(50) NOT NULL,
 	title varchar(100) NOT NULL,
 	birth_date date,
@@ -11,7 +11,7 @@ CREATE TABLE employees
 
 CREATE TABLE customers
 (
-	customer_id varchar(50) PRIMARY KEY,
+	customer_id varchar(30) PRIMARY KEY,
 	company_name varchar(100) NOT NULL,
 	contact_name varchar(100) NOT NULL
 );
@@ -21,7 +21,7 @@ CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
 	customer_id varchar(30) REFERENCES customers(customer_id),
-	employee_id int NOT NULL,
+	employee_id int NOT NULL ,
 	order_date date NOT NULL,
-	ship_city varchar(100) NOT NULL
+	ship_city varchar(15) NOT NULL
 );
