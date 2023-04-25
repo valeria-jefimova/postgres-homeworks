3c5220079bfb98bc128eee01b6856f089d6216f1
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-04-25 21:08:50 +0300

    updated filter_sorting.sql

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -3,7 +3,7 @@
 SELECT * FROM orders WHERE ship_country IN ('France', 'Germany', 'Spain')
 
 -- 2. уникальные города и страны, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
-SELECT DISTINCT ship_city, ship_country FROM orders ORDER BY ship_сountry, ship_city
+SELECT DISTINCT ship_city, ship_country FROM orders ORDER BY ship_country, ship_city
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
 SELECT AVG(shipped_date - order_date) FROM orders WHERE ship_country = 'Germany';
